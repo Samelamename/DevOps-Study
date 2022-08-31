7ab8654e0def86b1e79293c1295bf6396602aae7
Samelamename <110189324+Samelamename@users.noreply.github.com>
2022-08-31 21:48:13 +0100

    Update main.tf

--- Terraformstudy/AWSPractice/main.tf
+++ Terraformstudy/AWSPractice/main.tf
@@ -53,6 +53,6 @@ resource "aws_security_group" "main" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDa8E+VrS87wXQycsXl0mZWsAokMuR6TGoiwyPAuqVNwUmk185J6XFc+/7hNPz2Uxw5Om31R8zPQjov2gzr7KHnDVh1LAfZODOaDvo+crsC/cFyu35V69lhiKO7gvjO8JeekAkIlyiaa9ZondyCjrQIYC4IDRCnC1QEs2o2a3vxlTFgACtVunc8j4u0UteL/dxFYriN9W45D1MCPtM8UZG8eb+luN8ODyoT/lGEfc06hEqkLwHo1zOT819lnw/SjF+YV4HfigZaHGxZslHBSUhzL9EZAf8zBJQQ1xour5C0RLT7D92lfsrMnTrPoKHmZxDy29oSuTTYig9a8QjdeiwR Adan Olaad@DaPc"
+  public_key = ""
 }
 
