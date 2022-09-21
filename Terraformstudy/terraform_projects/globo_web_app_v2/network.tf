0e954d05da16b17a592024bdcfe9dc11e221a9ba
Samelamename <adanolaad@gmail.com>
2022-09-21 16:26:21 +0100

    Utilised modules for consistency

---
+++ Terraformstudy/terraform_projects/globo_web_app_v2/network.tf
@@ -0,0 +1,82 @@
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING #
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  cidr = var.vpc_cidr_block
+
+  azs            = slice(data.aws_availability_zones.available.names, 0, (var.vpc_subnet_count))
+  public_subnets = [for subnet in range(var.vpc_subnet_count) : cidrsubnet(var.vpc_cidr_block, 8, subnet)]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = merge(local.common_tags, {
+    name = "${local.name_prefix}-vpc"
+  })
+}
+
+
+
+# SECURITY GROUPS #
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  name   = "nginx_alb_sg"
+  vpc_id = module.vpc.vpc_id
+
+  #Allow HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+
+}
+
+# Nginx security group 
+resource "aws_security_group" "nginx-sg" {
+  name   = "nginx_sg"
+  vpc_id = module.vpc.vpc_id
+
+  # HTTP access from VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
+
+
+
